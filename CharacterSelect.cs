ccfdca9e66fdd16ccf703af1cb76a8388f47fd40
Kawa-oneechan <kawaoneechan@gmail.com>
2019-05-23 11:25:34 +0200

    Split off title screen and character select
    And only *now* will I start on letting you actually select characters!

---
+++ CharacterSelect.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using Kawa.Json;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Kafe
+{
+	class CharacterSelect : DrawableGameComponent
+	{
+		private Texture2D stuff;
+		private int anim, numPlayers;
+		private int[] cursor;
+		private Character[] selection;
+
+		private Vector2[] positions = new[] { new Vector2(104, Kafe.Ground), new Vector2(484, Kafe.Ground) };
+		private int[] namePositions = new[] { 40, 340 };
+
+		public CharacterSelect(bool versus)
+			: base(Kafe.Me)
+		{
+			stuff = Mix.GetTexture("menu.png");
+			cursor = new[] { 1, 0 };
+			selection = new[] { Kafe.Characters[1], Kafe.Characters[0] };
+			numPlayers = versus ? 2 : 1;
+			Kafe.CanExit = false;
+		}
+
+		public override void Update(GameTime gameTime)
+		{
+			base.Update(gameTime);
+			if (anim < 128)
+				anim++;
+
+			for (var i = 0; i < numPlayers; i++)
+			{
+				var control = Input.Controls[i];
+				if (control.TrgLeft)
+				{
+					cursor[i]--;
+					if (cursor[i] < 0)
+						cursor[i] = Kafe.Characters.Length - 1;
+					selection[i] = Kafe.Characters[cursor[i]];
+				}
+				else if (control.TrgRight)
+				{
+					cursor[i]++;
+					if (cursor[i] >= Kafe.Characters.Length)
+						cursor[i] = 0;
+					selection[i] = Kafe.Characters[cursor[i]];
+				}
+				else if (control.TrgUp)
+				{
+					cursor[i] -= 5;
+					if (cursor[i] < 0)
+						cursor[i] = Kafe.Characters.Length - 1;
+					selection[i] = Kafe.Characters[cursor[i]];
+				}
+				else if (control.TrgDown)
+				{
+					cursor[i] += 5;
+					if (cursor[i] >= Kafe.Characters.Length)
+						cursor[i] = 0;
+					selection[i] = Kafe.Characters[cursor[i]];
+				}
+			}
+		}
+
+		public override void Draw(GameTime gameTime)
+		{
+			base.Draw(gameTime);
+			var batch = Kafe.SpriteBatch;
+			batch.Begin();
+
+			var x = 176;
+			var y = 56;
+			var dst = new Rectangle(x, y, 25, 25);
+			var src = new Rectangle(16, 50, 25, 25);
+			for (var i = 0; i < Kafe.Characters.Length; i++)
+			{
+				batch.Draw(stuff, dst, src, Color.White);
+				dst.Inflate(-1, -1);
+				Kafe.Characters[i].DrawIcon(batch, dst, false);
+				dst.Inflate(1, 1);
+				dst.Offset(24, 0);
+				if (i % 5 == 4)
+					dst.Offset(-(5 * 24), 24);
+			}
+			dst = new Rectangle(x - 6, y - 6, 36, 36);
+			src = new Rectangle(42, 50, 36, 36);
+			for (var i = 0; i < Kafe.Characters.Length; i++)
+			{
+				if (i == cursor[0])
+					batch.Draw(stuff, dst, src, Color.Lime);
+				if (numPlayers > 1 && i == cursor[1])
+					batch.Draw(stuff, dst, src, Color.Red);
+				dst.Offset(24, 0);
+				if (i % 5 == 4)
+					dst.Offset(-(5 * 24), 24);
+			}
+			batch.End();
+
+			for (var i = 0; i < numPlayers; i++)
+			{
+				selection[i].Position = positions[i];
+				selection[i].FacingLeft = (i == 1);
+				selection[i].SelectMode = true;
+				selection[i].Update();
+				batch.Begin();
+				selection[i].PreDraw();
+				//selection[i].DrawShadow(batch);
+				Text.Draw(batch, 1, selection[i].Name, namePositions[i], 80);
+				batch.End();
+				selection[i].Draw(batch);
+			}
+
+		}
+	}
+}
---
+++ TitleScreen.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using Kawa.Json;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Kafe
+{
+	class TitleBackground : DrawableGameComponent
+	{
+		private Texture2D sheet, backdrop;
+		private int lineAnim, backdropAnim;
+
+		private static int gridStart = 49;
+
+		public TitleBackground()
+			: base(Kafe.Me)
+		{
+			sheet = Mix.GetTexture("menu.png");
+			backdrop = Mix.GetTexture("menu_back.png");
+		}
+
+		public override void Update(GameTime gameTime)
+		{
+			base.Update(gameTime);
+			backdropAnim++;
+			if (backdropAnim >= backdrop.Width * 16)
+				backdropAnim = 0;
+			lineAnim++;
+			if (lineAnim == 64)
+				lineAnim = 0;
+		}
+
+		public override void Draw(GameTime gameTime)
+		{
+			base.Draw(gameTime);
+			var batch = Kafe.SpriteBatch;
+			batch.Begin();
+			var dst = new Rectangle(-(backdropAnim / 16), 0, backdrop.Width, Kafe.ScreenHeight);
+			batch.Draw(backdrop, dst, Color.White);
+			dst.Offset(backdrop.Width, 0);
+			batch.Draw(backdrop, dst, Color.White);
+			var src = new Rectangle(0, 0, 384, 49);
+			dst = new Rectangle(0, Kafe.ScreenHeight - gridStart, Kafe.ScreenWidth, gridStart);
+			batch.Draw(sheet, dst, src, Color.White);
+			src = new Rectangle(1, 50, 14, 14);
+			dst = new Rectangle(0, Kafe.ScreenHeight - gridStart + (lineAnim / 4), Kafe.ScreenWidth, 14);
+			for (var i = 0; i < 4; i++)
+			{
+				batch.Draw(sheet, dst, src, Color.White);
+				dst.Offset(0, 16);
+			}
+			batch.End();
+		}
+	}
+
+	class TitleScreen : DrawableGameComponent
+	{
+		private Texture2D titleE, titleF;
+		private int anim, selection;
+
+		public TitleScreen()
+			: base(Kafe.Me)
+		{
+			titleE = Mix.GetTexture("title_e.png");
+			titleF = Mix.GetTexture("title_f.png");
+			Kafe.CanExit = true;
+			selection = 0;
+		}
+
+		public override void Update(GameTime gameTime)
+		{
+			base.Update(gameTime);
+			if (anim < 128)
+				anim++;
+
+			if (Input.WasJustReleased(Keys.Enter))
+			{
+				Input.Flush();
+				switch (selection)
+				{
+					case 0: //Story Mode
+						Kafe.DoTransition(false, () =>
+						{
+							Kafe.Me.Components.Remove(this);
+							LoadingScreen.Start(() =>
+							{
+								var charSelect = new CharacterSelect(false) { Enabled = false };
+								Kafe.Me.Components.Add(charSelect);
+								Kafe.DoTransition(true, () => { charSelect.Enabled = true; });
+							});
+						});
+						break;
+					case 1: //Versus Mode
+						Kafe.DoTransition(false, () =>
+						{
+							Kafe.Me.Components.Remove(this);
+							LoadingScreen.Start(() =>
+							{
+								var charSelect = new CharacterSelect(true) { Enabled = false };
+								Kafe.Me.Components.Add(charSelect);
+								Kafe.DoTransition(true, () => { charSelect.Enabled = true; });
+							});
+						});
+						break;
+					case 2: //Quit
+						Kafe.ExitConfirm = true;
+						ConfirmScreen.Ask("Are you sure you want to exit?", () => { Kafe.Me.Exit(); }, () => { Kafe.ExitConfirm = Kafe.Paused = false; Input.Flush(); });
+						break;
+				}
+				//LoadingScreen.Start(() => { Kafe.Me.Components.Add(new CharacterSelectScreen()); });
+			}
+		}
+
+		public override void Draw(GameTime gameTime)
+		{
+			base.Draw(gameTime);
+			var batch = Kafe.SpriteBatch;
+			batch.Begin();
+			var dst = new Vector2(Kafe.ScreenWidth / 2, Kafe.ScreenHeight / 2);
+			var center = new Vector2(titleE.Width / 2, titleE.Height / 2);
+
+			batch.Draw(titleF, dst, null, Color.White, 0.0f, center, (anim < 32) ? anim / 32f : 1f, SpriteEffects.None, 0);
+			if (anim > 64)
+				batch.Draw(titleE, dst, null, Color.White, 0.0f, center, (anim < 96) ? 2f - ((anim - 64) / 32f) : 1f, SpriteEffects.None, 0);
+
+			batch.End();
+		}
+	}
+}
